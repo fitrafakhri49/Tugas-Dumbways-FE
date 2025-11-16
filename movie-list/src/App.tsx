0ff29ecfc452f3d34df4b971ebc74bf46a3e167f
Fakhri Fitra <fitrafakhri49@gmail.com>
2025-11-16 18:20:17 +0700

    Final

--- movie-list/src/App.tsx
+++ movie-list/src/App.tsx
@@ -51,8 +51,6 @@ function App() {
           <Route path="/about" element={<About/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/movies" element={<Movies/>}/>
-
-          {/* Private Route */}
           <Route
             path="/favorites"
             element={
