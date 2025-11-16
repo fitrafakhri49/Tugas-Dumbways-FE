ca5ef8222fa9a3cb0c32f7ee39d04284c5436187
Fakhri Fitra <fitrafakhri49@gmail.com>
2025-11-16 12:28:23 +0700

    first commit

---
+++ product-catalog/src/pages/Login.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useAuth } from "@/hooks/useAuth";
+import { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
+export function Login() {
+    const {login}=useAuth()
+    const navigate=useNavigate();
+    const [username,setUsername]=useState("")   
+    const [password,setPassword]=useState("")   
+    const [errorMessage,setErrorMessage]=useState("")   
+
+    const handleLogin=(e:React.FormEvent)=>{
+        e.preventDefault();
+    
+if(username== "admin" && password =="admin"){
+    login("token_abc") 
+    navigate("/products")
+}
+else{
+    setErrorMessage("username atau password salah")
+}
+    }
+    return(
+        <div className="flex items-center justify-center min-h-screen p-4 ">
+            <form  onSubmit={handleLogin} className="w-full max-sm bg-white dark:bg-zinc-900 p-6 rounded shadow space-y-4">
+                <h1 className="text-2xl font-bold text-center">Login</h1>
+                <div className="">
+                    <Label htmlFor="username">Username</Label>
+                    <Input
+                    id="username" type="text" placeholder="username" value={username} onChange={(event)=>setUsername(event.target.value)}/>
+                </div>
+
+                <div className="">
+                    <Label htmlFor="password">Password</Label>
+                    <Input
+                    id="password" type="password" placeholder="password" value={password} onChange={(event)=>setPassword(event.target.value)} required/>
+                </div>
+                {errorMessage &&(
+                    <p>{errorMessage}</p>
+                )}
+                <Button type="submit" className="w-full">Login</Button>
+            </form>
+        </div>
+    )
+}
---
+++ product-catalog/src/pages/Products.tsx
@@ -0,0 +1,85 @@
+import { Link } from "react-router-dom"
+import { useState,useEffect } from "react";
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import { api } from "@/services/api";
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+type ProductType ={
+    id:number;
+    title:string;
+    description:string;
+}
+
+
+export default function Posts(){
+
+    const [products,setProducts]=useState<ProductType[]>([]);
+    const [loading,setLoading]=useState(true)
+    const [selectedProduct,setSelectedProduct]=useState<ProductType | null>(null)
+
+    useEffect(()=>{
+        const fetchData=async ()=>{
+            try {
+                const res = await api.get("/products")
+                setProducts(res.data)
+            } catch (error) {
+                console.error("Gagal Fetch Data Produk")
+            }finally{
+                setLoading(false)
+            }
+        }
+        fetchData()
+    },[])
+    return(
+        <div className="p-4">
+
+        <h1 className="text-5xl mb-4 font-bold">Products</h1>
+
+        {loading ? (
+            <p className="text-center">Loading.....</p>
+        ) :(
+          <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-4 ">
+        {products.map((product)=>(
+            <Dialog key={product.id}>
+                <DialogTrigger asChild>
+                <Card onClick={()=>setSelectedProduct(product)} className="cursor-pointer hover:shadow-md transition">
+                <CardHeader>
+                    <CardTitle>{product.title}</CardTitle>
+                    <CardDescription className="truncate">{product.description}</CardDescription>
+                    </CardHeader>    
+                    </Card>                
+                    </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>{selectedProduct?.title}</DialogTitle>
+                        <DialogDescription>
+                            {selectedProduct?.description}
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+            ))}
+            </ul> 
+        )}
+            
+        </div>
+    )
+}
+
+
